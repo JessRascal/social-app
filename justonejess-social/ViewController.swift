d45042158ff7930f1f05926c7535aec364e99e6d
Jess Rascal <Jess@Jessey.uk>
2016-04-25 12:49:01 +0100

    Initial Commit

---
+++ justonejess-social/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  justonejess-social
+//
+//  Created by Jess Rascal on 25/04/2016.
+//  Copyright © 2016 JustOneJess. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
